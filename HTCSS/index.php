4d8f2190a21afda71fdd30189c2d39989d28c578
j4kelineflorencio <113375243+j4kelineflorencio@users.noreply.github.com>
2023-09-23 07:28:39 -1100

    Create index.php

---
+++ HTCSS/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Green Solves</title>
+</head>
+<body>
+    <div class="main-login">
+        <div class="left-login">
+            <img src="img/green.svg" class="left-login-image"alt="Logo do Green Solves">
+        </div>
+        <div class="right-login">
+         <div class="card-login">
+            <h1>Bem-vindo(a)!</h1>
+            <div class="textfield">
+                 <label for ="usuario">Email</label>
+                 <input type ="text" name ="email" placeholder="Email">
+            </div>
+            <div class="textfield">
+                <label for ="senha">Senha</label>
+                <input type ="password" name ="senha" placeholder="Senha">
+                <h5>Esqueceu sua senha?</h5>
+           </div>
+           <button class="btn-login">Entrar</button>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
